5e5e654bc470b0bb7f3a418bd31c63ec103fa138
Ric4j4 <richardwijaya31@gmail.com>
2024-05-11 12:30:55 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,37 @@
 import React, { useState } from "react";
 import Navbar from "./components/Navbar";
+import Footer from "./components/footer";
+import videoBg from "./video/VideoBabel.mp4";
 import "./styles.css";
 
 export default function App() {
   const [showNavbar, setShowNavbar] = useState(false);
-  const [showAdditionalContent, setShowAdditionalContent] = useState(false);
+  const [showFooter, setShowFooter] = useState(false);
+  const [showButton, setShowButton] = useState(true);
 
   const handleExploreClick = () => {
     setShowNavbar(!showNavbar);
-    setShowAdditionalContent(!showNavbar);
+    setShowFooter(!showFooter);
+    setShowButton(false);
   };
 
   return (
     <div className="App">
       {showNavbar && <Navbar />}
-      <h1 className="Name">Bangka Belitung</h1>
-      <button className="Explore" onClick={handleExploreClick}>
-        Explore Now
-      </button>
-      {showAdditionalContent && (
-        <table className="AboutUs">
-          <div className="AboutUss">
-            <ul className="Bottom">
-              <li className="PT">
-                <h2>PT. BabelTrip Indonesia</h2>
-              </li>
-              <li>
-                <div className="Social">
-                  <i id="Facebook" className="fa-brands fa-facebook"></i>
-                  <i id="Instagram" className="fa-brands fa-instagram"></i>
-                  <i id="Gmail" className="fa-solid fa-envelope"></i>
-                </div>
-              </li>
-            </ul>
-            <div className="Bot">
-              <br />
-              <p>Scientia Boulevard</p>
-              <p>Gading Serpong, Tangerang</p>
-              <p>Tel : (021) 123456789</p>
-              <br />
-              <p>Copyright © 2024 BabelTrip. All rights reserved.</p>
-            </div>
-          </div>
-        </table>
-      )}
+      <div className="videoContainer">
+        <video className="VideoBackground" src={videoBg} autoPlay loop muted />
+        <ul className="content">
+          <li>
+            <h1 className="Name">Bangka Belitung</h1>
+            {showButton && ( 
+              <button className="Explore" onClick={handleExploreClick}>
+                Explore Now
+              </button>
+            )}
+          </li>
+        </ul>
+      </div>
+      {showFooter && <Footer />}
     </div>
   );
 }
