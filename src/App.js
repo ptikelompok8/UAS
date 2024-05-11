ce2b3cd4e72db13e7775ab25074e90571723afa0
Ric4j4 <richardwijaya31@gmail.com>
2024-05-11 09:12:29 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
-import "./styles.css";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
+import "./styles.css";
 
 export default function App() {
+  const [showNavbar, setShowNavbar] = useState(false);
+  const [showAdditionalContent, setShowAdditionalContent] = useState(false);
+
+  const handleExploreClick = () => {
+    setShowNavbar(!showNavbar);
+    setShowAdditionalContent(!showNavbar);
+  };
+
   return (
     <div className="App">
-      <Navbar/>
+      {showNavbar && <Navbar />}
       <h1 className="Name">Bangka Belitung</h1>
-      <button className="Explore ">Explore Now</button>
-      <table className="AboutUs">
-        <div className="AboutUss">
-                <h2>PT. Babeltrip Indonesia</h2>
-                <br></br>
-                <p>Scientia Boulevard</p>
-                <p>Gading Serpong, Tangerang</p>
-                <p>Tel : (021) 123456789</p>
-                <br></br>
+      <button className="Explore" onClick={handleExploreClick}>
+        Explore Now
+      </button>
+      {showAdditionalContent && (
+        <table className="AboutUs">
+          <div className="AboutUss">
+            <ul className="Bottom">
+              <li className="PT">
+                <h2>PT. BabelTrip Indonesia</h2>
+              </li>
+              <li>
                 <div className="Social">
-                <i id="Facebook" class="fa-brands fa-facebook"></i>
-                <i id="Instagram" class="fa-brands fa-instagram"></i>
-                <i id="Gmail" class="fa-solid fa-envelope"></i>
+                  <i id="Facebook" className="fa-brands fa-facebook"></i>
+                  <i id="Instagram" className="fa-brands fa-instagram"></i>
+                  <i id="Gmail" className="fa-solid fa-envelope"></i>
                 </div>
+              </li>
+            </ul>
+            <div className="Bot">
+              <br />
+              <p>Scientia Boulevard</p>
+              <p>Gading Serpong, Tangerang</p>
+              <p>Tel : (021) 123456789</p>
+              <br />
+              <p>Copyright © 2024 BabelTrip. All rights reserved.</p>
+            </div>
           </div>
-      </table>
+        </table>
+      )}
     </div>
   );
 }
