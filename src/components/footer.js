715d10e35a4adcfdf04b4213bd9c395b73e67581
Ric4j4 <richardwijaya31@gmail.com>
2024-05-11 12:25:28 +0700

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,36 +1,32 @@
-import { Component } from "react";
+import React from "react";
 import "./FooterStyles.css";
 
-class Footer extends Component {
-    render(){
-        return (
-            <table className="AboutUs">
-                    <div className="AboutUss">
-                    <ul className="Bottom">
-                        <li className="PT">
-                        <h2>PT. BabelTrip Indonesia</h2>
-                        </li>
-                        <li>
-                        <div className="Social">
-                            <i id="Facebook" className="fa-brands fa-facebook"></i>
-                            <i id="Instagram" className="fa-brands fa-instagram"></i>
-                            <i id="Gmail" className="fa-solid fa-envelope"></i>
-                        </div>
-                        </li>
-                    </ul>
-                    <div className="Bot">
-                        <br />
-                        <p>Scientia Boulevard</p>
-                        <p>Gading Serpong, Tangerang</p>
-                        <p>Tel : (021) 123456789</p>
-                        <br />
-                        <p>Copyright © 2024 BabelTrip. All rights reserved.</p>
-                    </div>
-                    </div>
-                </table>
-                );
-            }
-        }
-          
+function Footer({ showAdditionalContent }) {
+  return (
+    <table className="AboutUs">
+    <div className="AboutUss">
+    <ul className="Bottom">
+        <li className="PT">
+            <h2>PT. BabelTrip Indonesia</h2>
+        </li>
+        <li>
+            <div className="Social">
+            <i id="Facebook" className="fa-brands fa-facebook"></i>
+            <i id="Instagram" className="fa-brands fa-instagram"></i>
+            <i id="Gmail" className="fa-solid fa-envelope"></i>
+            </div>
+        </li>
+    </ul>
+      <div className="Bot">
+        <br />
+        <p>Scientia Boulevard</p>
+        <p>Gading Serpong, Tangerang</p>
+        <p>Tel : (021) 123456789</p>
+        <br />
+        <p>Copyright © 2024 BabelTrip. All rights reserved.</p>
+      </div>
+    </div>
+  </table>
+  )};
+
 export default Footer;
-          
