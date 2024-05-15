0ed14f0c9c1524e171db67fdbfb68f40cc9309e0
Ric4j4 <richardwijaya31@gmail.com>
2024-05-15 11:04:56 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,29 @@
 import React, { useState } from "react";
-import Navbar from "./components/Navbar";
-import Footer from "./components/footer";
-import videoBg from "./video/VideoBabel.mp4";
 import "./styles.css";
+import Navbar from "./components/Navbar";
+import Review from "./routes/review";
+import { Home } from "./routes/Home";
+import Footer from "./components/footer";
 
 export default function App() {
-  const [showNavbar, setShowNavbar] = useState(false);
-  const [showFooter, setShowFooter] = useState(false);
-  const [showButton, setShowButton] = useState(true);
+  let component
+  switch(window.location.pathname){
+    case "/":
+      component = <Home />
+      break
+    case "/service": 
+      component = <Review />
+      break
 
-  const handleExploreClick = () => {
-    setShowNavbar(!showNavbar);
-    setShowFooter(!showFooter);
-    setShowButton(false);
-  };
-
+  }
   return (
-    <div className="App">
-      {showNavbar && <Navbar />}
-      <div className="videoContainer">
-        <video className="VideoBackground" src={videoBg} autoPlay loop muted />
-        <ul className="content">
-          <li>
-            <h1 className="Name">Bangka Belitung</h1>
-            {showButton && ( 
-              <button className="Explore" onClick={handleExploreClick}>
-                Explore Now
-              </button>
-            )}
-          </li>
-        </ul>
-      </div>
-      {showFooter && <Footer />}
-    </div>
-  );
+    <>
+      <Navbar/>
+      {component}
+      <Footer/>
+    </>
+  )
 }
+
+
 
