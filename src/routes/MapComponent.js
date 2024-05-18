4f26a10786bcfbbc63178ad5cc322da74ab78f4d
Ric4j4 <richardwijaya31@gmail.com>
2024-05-18 08:46:39 +0700

    Update MapComponent.js

--- src/routes/MapComponent.js
+++ src/routes/MapComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 
 const containerStyle = {
   width: '100%',
@@ -7,11 +7,19 @@ const containerStyle = {
 };
 
 const center = {
-  lat: -2.741051,
-  lng: 106.440587
+  lat: -2.133333,
+  lng: 106.116669
 };
 
+const locations = [
+  { name: 'Tikus Beach', position: { lat: -1.885932568659634, lng: 106.17590183521659 } },
+  { name: 'Tanjung Tinggi Beach', position: { lat: -2.551686542833416, lng: 107.7137724453313 } },
+  { name: 'Pulau Lengkuas', position: { lat: -2.5323899939506287, lng: 107.60042907930213 } }
+];
+
 const Map = () => {
+  const [selected, setSelected] = React.useState(null);
+
   return (
     <LoadScript
       googleMapsApiKey="AIzaSyCHTlvuDTn2Oyr6b5CWbCPR2_OqwyHZ2o0"
@@ -22,6 +30,22 @@ const Map = () => {
         zoom={10}
       >
         <Marker position={center} />
+        {locations.map((location, index) => (
+          <Marker
+            key={index}
+            position={location.position}
+            onClick={() => setSelected(location)}
+          />
+        ))}
+
+        {selected && (
+          <InfoWindow
+            position={selected.position}
+            onCloseClick={() => setSelected(null)}
+          >
+            <div>{selected.name}</div>
+          </InfoWindow>
+        )}
       </GoogleMap>
     </LoadScript>
   );
